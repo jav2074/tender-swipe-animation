64e63c966052505675cd787943c651c4d04a1460
jav2074 <jav.electrochance@gmail.com>
2023-07-06 14:15:06 -0300

    add primera version

--- js/script.js
+++ js/script.js
@@ -1,58 +1,103 @@
-var app = angular.module('myApp', []);
-// configure our routes
-app.config(function ($httpProvider)
-{
-  $httpProvider.defaults.headers.common = {};
-  $httpProvider.defaults.headers.post = {};
-  $httpProvider.defaults.headers.put = {};
-  $httpProvider.defaults.headers.patch = {};
-  $httpProvider.defaults.headers.get = {};
-  $httpProvider.defaults.headers.common['Access-Control-Allow-Headers'] = '*';
-  $httpProvider.defaults.useXDomain = true;
-  delete $httpProvider.defaults.headers.common['X-Requested-With'];
-});
-// controller
-app.controller('myCtrl', function($scope, $http)
-{
-  // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-    // let urlFake_01 = "https://testamp2023.000webhostapp.com/php/amp.php";
-    // let urlFake_01 = "/php/amp.php";
-    // httpGet($http, urlFake_01, callBackFake_01);
-    // function callBackFake_01(result)
-    // {
-    //     $scope.urlFake_01 = result;
-    // }
-  // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-  $scope.pics = [
-    { class:"active", image: 'https://img.freepik.com/foto-gratis/hermosa-mujer-elegante-sonriendo-mirando-interesado_176420-19460.jpg', alt:"foto", width:"600", height:"424"},
-    { class:"", image: 'https://img.freepik.com/fotos-premium/hermosa-mujer-joven-encantadora-pelo-largo-rizado-morena-camisa-blanca-que-encuentran-aisladas-sobre-pared-blanca_171337-98697.jpg', alt:"foto", width:"600", height:"424"},
-    { class:"", image: 'https://img.freepik.com/fotos-premium/hermosa-mujer-cabello-largo-oscuro-telefono-inteligente-llamada-movil_171337-31572.jpg', alt:"foto", width:"600", height:"424"},
-    { class:"", image: 'https://img.freepik.com/foto-gratis/retrato-belleza-misterio-sonriente-mujer-jengibre-cabello-largo-posando-lado-mirando_171337-920.jpg', alt:"foto", width:"600", height:"424"},
-    { class:"", image: "http://www.bcu.ac.uk/mediaitem/visual-communication-illustration-02-130585258441045504.jpg?quality=90", alt:"foto", width:"600", height:"424"},
-    { class:"", image: "https://mir-s3-cdn-cf.behance.net/project_modules/1400/e86e8b59124321.5a15e4ccd8622.jpg", alt:"foto", width:"600", height:"424"},
-    { class:"", image: "https://cdn.dribbble.com/users/324248/screenshots/3978513/planet2_800.jpg", alt:"foto", width:"600", height:"424"}
-  ];
-  $scope.data = {
-    name: 'Romy',
-    age: 23,
-    about: '',
-    gener: '',
-    altura: 165,
-    smoke: false,
-    drink:'',
-    sign: '',
-    education:'',
-    interest:'',
-    religion:'',
-    chailds:'',
-    location: '',
-    search:''
-  };
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+let imgCount = 0
+const cloudUrl = 'https://djjjk9bjm164h.cloudfront.net/'
+const data = [
+  {img: `${cloudUrl}tender01.jpg`, name: 'Korean Fried', price: '20', distance: '2'},
+  {img: `${cloudUrl}tender02.jpg`, name: 'Grilled', price: '23', distance: '5'},
+  {img: `${cloudUrl}tender03.jpg`, name: 'Fried', price: '25', distance: '11'},
+  {img: `${cloudUrl}tender04.jpg`, name: 'Deep Fried', price: '23', distance: '6'}
+]
+const frame = document.body.querySelector('.frame')
+data.forEach(_data => appendCard(_data))
 
+let current = frame.querySelector('.card:last-child')
+let likeText = current.children[0]
+let startX = 0, startY = 0, moveX = 0, moveY = 0
+initCard(current)
 
-  $scope.likeOrNot = function(like_) {
-    console.log('Me gusta: ' + like_);
-  };
-  // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+document.querySelector('#like').onclick = () => {
+  moveX = 1
+  moveY = 0
+  complete()
+}
+document.querySelector('#hate').onclick = () => {
+  moveX = -1
+  moveY = 0
+  complete()
+}
 
-});
+function appendCard(data) {
+  const firstCard = frame.children[0]
+  const newCard = document.createElement('div')
+  newCard.className = 'card'
+  newCard.style.backgroundImage = `url(${data.img})`
+  newCard.innerHTML = `
+          <div class="is-like">LIKE</div>
+          <div class="bottom">
+            <div class="title">
+              <span>${data.name}</span>
+              <span><b>$</b>${data.price}</span>
+            </div>
+            <div class="info">
+              ${data.distance} miles away
+            </div>
+          </div>
+        `
+  if (firstCard) frame.insertBefore(newCard, firstCard)
+  else frame.appendChild(newCard)
+  imgCount++
+}
+
+function initCard(card) {
+  card.addEventListener('pointerdown', onPointerDown)
+}
+
+function setTransform(x, y, deg, duration) {
+  current.style.transform = `translate3d(${x}px, ${y}px, 0) rotate(${deg}deg)`
+  likeText.style.opacity = Math.abs((x / innerWidth * 2.1))
+  likeText.className = `is-like ${x > 0 ? 'like' : 'nope'}`
+  if (duration) current.style.transition = `transform ${duration}ms`
+}
+
+function onPointerDown({ clientX, clientY }) {
+  startX = clientX
+  startY = clientY
+  current.addEventListener('pointermove', onPointerMove)
+  current.addEventListener('pointerup', onPointerUp)
+  current.addEventListener('pointerleave', onPointerUp)
+}
+
+function onPointerMove({ clientX, clientY }) {
+  moveX = clientX - startX
+  moveY = clientY - startY
+  setTransform(moveX, moveY, moveX / innerWidth * 50)
+}
+
+function onPointerUp() {
+  current.removeEventListener('pointermove', onPointerMove)
+  current.removeEventListener('pointerup', onPointerUp)
+  current.removeEventListener('pointerleave', onPointerUp)
+  if (Math.abs(moveX) > frame.clientWidth / 2) {
+    current.removeEventListener('pointerdown', onPointerDown)
+    complete()
+  } else cancel()
+}
+
+function complete() {
+  const flyX = (Math.abs(moveX) / moveX) * innerWidth * 1.3
+  const flyY = (moveY / moveX) * flyX
+  setTransform(flyX, flyY, flyX / innerWidth * 50, innerWidth)
+
+  const prev = current
+  const next = current.previousElementSibling
+  if (next) initCard(next)
+  current = next
+  likeText = current.children[0]
+  appendCard(data[imgCount % 4])
+  setTimeout(() => frame.removeChild(prev), innerWidth)
+}
+
+function cancel() {
+  setTransform(0, 0, 0, 100)
+  setTimeout(() => current.style.transition = '', 100)
+}
